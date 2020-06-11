9aeec5feb5f0af00491a5aaa0f2ea339bbc6bdbe
Teemo Tebest <teemo.tebest@gmail.com>
2020-06-11 12:53:25 +0200

    Removed trailing slash

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -164,7 +164,7 @@ class App extends Component {
       path_prefix = './';
     }
     else {
-      path_prefix = 'https://raw.githubusercontent.com/ebuddj/2020-covid19stories/public//';
+      path_prefix = 'https://raw.githubusercontent.com/ebuddj/2020-covid19stories/public/';
     }
 
     
